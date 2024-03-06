5f877b76a2ae87b43b3a6341ae3f7597e8d38057
Virgilio <virgiliosanchez3435@gmail.com>
2024-03-05 18:27:29 -0600

    Solucion de uso de metodos en otras clases

--- Main.java
+++ Main.java
@@ -1,40 +1,36 @@
-import expresiones;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Main {
-	
-	private static File archivoEntrada;
-	private static File archivoSalida;
-	public static ArrayList<String> listaDeResultados = new ArrayList<>();
-	
-	//Menu
+
+    public static ArrayList<String> listaDeResultados = new ArrayList<>();
+
+    public static opciones archivo = new opciones();
+    public static expresiones lenguajes = new expresiones();
+
+    // Menu
     public static void main(String[] args) {
 
         String menu = "1. Ingresar cadena" + //
-                "\n2. Leer archivo de entrada"+ //lee el archivo de entrada con las cadenas a procesar
-                "\n3. Mostrar resultados en memoria"+
-                "\n4. Limpiar resultados en memoria"+
-                "\n5. Guardar resultados en archivo de salida"+ //Unica manera de guardar resultados en archivo de texto
-                "\n6. Definir direccion de archivo entrada"+
+                "\n2. Leer archivo de entrada" + // lee el archivo de entrada con las cadenas a procesar
+                "\n3. Mostrar resultados en memoria" +
+                "\n4. Limpiar resultados en memoria" +
+                "\n5. Guardar resultados en archivo de salida" + // Unica manera de guardar resultados en archivo de
+                                                                 // texto
+                "\n6. Definir direccion de archivo entrada" +
                 "\n7. Definir direccion de archivo salida";
 
         char opcion;
-        
-         //Almacena los resultados obtenidos de las cadenas procesadas
-        
-        String input;
-        
-        do {
-             input = JOptionPane.showInputDialog(null, menu);
 
-            // Si el usuario hace clic en "Cancelar" o cierra la ventana, salimos del programa
+        // Almacena los resultados obtenidos de las cadenas procesadas
+
+        String input;
+
+        do {
+            input = JOptionPane.showInputDialog(null, menu);
+
+            // Si el usuario hace clic en "Cancelar" o cierra la ventana, salimos del
+            // programa
             if (input == null) {
                 System.exit(0);
             }
@@ -42,46 +38,43 @@ public class Main {
             opcion = input.charAt(0);
 
             switch (opcion) {
-            	case '1':
-            		String sCadena = JOptionPane.showInputDialog("Ingrese cadena: ");
-            		String sResult = getResult(sCadena);
-            		listaDeResultados.add(sResult);
-            		JOptionPane.showMessageDialog(null, sResult);
-            		break;
-            		
+                case '1':
+                    String sCadena = JOptionPane.showInputDialog("Ingrese cadena: ");
+                    String sResult = lenguajes.getResult(sCadena);
+                    listaDeResultados.add(sResult);
+                    JOptionPane.showMessageDialog(null, sResult);
+                    break;
+
                 case '2':
-                	leerArchivo();
-                	
+                    archivo.leerArchivo();
                     break;
+
                 case '3':
-                	mostrarResultados();
-                 	break;
+                    archivo.mostrarResultados();
+                    break;
+
                 case '4':
-                	listaDeResultados.clear();
-                	JOptionPane.showMessageDialog(null, "Lista de resultados limpiada");
-                	break;
+                    listaDeResultados.clear();
+                    JOptionPane.showMessageDialog(null, "Lista de resultados limpiada");
+                    break;
+
                 case '5':
-                	escribirEnArchivo();
-                	break;
+                    archivo.escribirEnArchivo();
+                    break;
+
                 case '6':
-                	setArchivoEntrada();
+                    archivo.setArchivoEntrada();
                     break;
+
                 case '7':
-                	setArchivoSalida();
+                    archivo.setArchivoSalida();
                     break;
+                    
                 default:
                     JOptionPane.showMessageDialog(null, "Vuelve a intentar");
                     break;
             }
         } while (input != null);
     }
-    
-    
-    
-    
-    
-    
-    
-  
-    
-  }
+
+}
