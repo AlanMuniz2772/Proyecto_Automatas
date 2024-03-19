ff4b5b0b15ce10ffcd60002e39914a4ba410571b
Alan <jorgeamr2772@gmail.com>
2024-03-19 11:27:36 -0600

    ultimo cambio numeros

--- expresiones.java
+++ expresiones.java
@@ -140,7 +140,7 @@ public class expresiones {
 
     public static String analizarNumeros(String sCadena) {
         // Expresión regular para identificar números del 0 al 9
-        String regexNumeros = "-?\\d+(\\.\\d+)?[dlf]";
+        String regexNumeros = "-?\\d+(\\.\\d+)?[dlf]?";
 
         if(Pattern.matches(regexNumeros, sCadena)){
             return "\nLa cadena es un numero en lenguaje C";
